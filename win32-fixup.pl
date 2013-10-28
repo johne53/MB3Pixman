5a527693f2a29e45972f31f3ee6491598a9e2c42
John Emmas <johne53@tiscali.co.uk>
2013-10-28 14:00:01 +0000

    JOHNE - Preliminary stuff for generating pkg-config files. Will probably get supplemented depending upon feedback from Todd

--- win32-fixup.pl
+++ win32-fixup.pl
@@ -3,12 +3,14 @@
 
 require "../local-paths.lib";
 
+$pixman_version = "0.31.1";
 $major = 0;
 $minor = 31;
 $micro = 1;
-$binary_age = 311;
+$binary_age = 3101;
 $interface_age = 0;
 $current_minus_age = 1;
+$exec_prefix = "lib";
 
 sub process_file
 {
@@ -25,6 +27,7 @@ sub process_file
 	    s/\@PIXMAN_BINARY_AGE\@/$binary_age/g;
 	    s/\@PIXMAN_INTERFACE_AGE\@/$interface_age/g;
 	    s/\@LT_CURRENT_MINUS_AGE\@/$current_minus_age/g;
+	    s/\@PACKAGE_VERSION@/$pixman_version/g;
 	    s/\@GlibBuildRootFolder@/$glib_build_root_folder/g;
 	    s/\@CairoBuildProjectFolder@/$cairo_build_project_folder/g;
 	    s/\@GenericIncludeFolder@/$generic_include_folder/g;
@@ -36,11 +39,16 @@ sub process_file
 	    s/\@Debug32TargetFolder@/$debug32_target_folder/g;
 	    s/\@Release32TargetFolder@/$release32_target_folder/g;
 	    s/\@TargetSxSFolder@/$target_sxs_folder/g;
+	    s/\@prefix@/$prefix/g;
+	    s/\@exec_prefix@/$exec_prefix/g;
+	    s/\@includedir@/$generic_include_folder/g;
+	    s/\@libdir@/$generic_library_folder/g;
 	    print OUTPUT;
 	}
 }
 
 process_file ("pixman/pixman-version.h");
+process_file ("pixman-1.pc");
 
 my $command=join(' ',@ARGV);
 if ($command eq -buildall) {
